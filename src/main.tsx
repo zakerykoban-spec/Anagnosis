a6a5d6548ca33aa4eb95e88563e8bbdacc654878
zakerykoban-spec <zakery.koban@gmail.com>
2026-07-25 14:54:57 -0400

    style: load meal prayer dock styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './dark-mode.css'
+import './meal-prayers.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
